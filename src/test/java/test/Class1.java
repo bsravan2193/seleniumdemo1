81febbcbaf558a7726c0ffd2997128eb397b31ee
sravan <sravan@DESKTOP-V7RFJ42>
2022-11-24 20:21:03 +0530

    hi , i am sravan

---
+++ src/test/java/test/Class1.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Class1 {
+
+	public static void main(String[] args) throws Exception 
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Thread.sleep(3000); 		
+		driver.quit();
+		
+
+	}
+
+}
